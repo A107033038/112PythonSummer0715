9a8051eb372d4f7686daa3aa4bf47227027de089
A107033038 <edwin991229@gmail.com>
2024-07-22 16:05:44 +0800

    113-07-22

--- 113-07-22/02-Karel/karel_07.py
+++ 113-07-22/02-Karel/karel_07.py
@@ -3,7 +3,11 @@ import os
 
 def main():
     """ Karel code goes here! """
+    while front_is_clear():
+        put_beeper()
+        move() 
+    put_beeper()
     pass
 
 if __name__ == "__main__":
-    run_karel_program(os.path.join(os.getcwd(), 'worlds/karel_07'))
+    run_karel_program(os.path.join(os.getcwd(), 'worlds/karel_06'))
